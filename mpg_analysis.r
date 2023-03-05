8f65da9d855a49ac0cfe4d7e372aa79a9420e19b
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-03-05 15:19:43 -0500

    added code

--- mpg_analysis.r
+++ mpg_analysis.r
@@ -4,5 +4,5 @@ conflicts_prefer(dplyr::filter())
 conflicts_prefer(dplyr::lag())
 
 View(mpg)
-ggplot(data = mpg) 
+ggplot(data = mpg) + geom_smooth(mapping = aes(x = displ, y = hwy, color = class))
 
